3c489978f92449b6e4a9961a3421369e985b60c1
Anovvoldigoad <you@example.com>
2026-09-23 17:57:30 +0700

    fix p87a p81 header staging

--- prepare_exlaunch.sh
+++ prepare_exlaunch.sh
@@ -7,6 +7,7 @@ cp "$ROOT/overlay/source/program/main.cpp" "$EXL/source/program/main.cpp"
 cp "$ROOT/overlay/source/program/nsc_cpk_bridge.cpp" "$EXL/source/program/nsc_cpk_bridge.cpp"
 cp "$ROOT/overlay/source/program/nsc_cpk_bridge.hpp" "$EXL/source/program/nsc_cpk_bridge.hpp"
 cp "$ROOT/overlay/source/program/condition_compat_generated.hpp" "$EXL/source/program/condition_compat_generated.hpp"
+cp "$ROOT/overlay/source/program/p81_ougi_awake_ids.hpp" "$EXL/source/program/p81_ougi_awake_ids.hpp"
 python3 - "$EXL/config.mk" <<'PY'
 from pathlib import Path
 import re, sys
